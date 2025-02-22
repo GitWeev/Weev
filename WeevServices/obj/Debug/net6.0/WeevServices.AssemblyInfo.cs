5c58e7b1c62a6fb37a1011d1b69ff9e8a9af3de5
GitWeev <167675644+GitWeev@users.noreply.github.com>
2025-02-22 13:50:16 +0530

    code merged

--- WeevServices/obj/Debug/net6.0/WeevServices.AssemblyInfo.cs
+++ WeevServices/obj/Debug/net6.0/WeevServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2ee2c86a4568cd808b4ebfd908e6de35ce70bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
