aea8ed90e2e7ea94b1cf443178654605088f5374
GitWeev <167675644+GitWeev@users.noreply.github.com>
2025-03-01 19:02:01 +0530

    db proc commit

--- DB Scripts/database-fixes.sql
+++ DB Scripts/database-fixes.sql
@@ -15,3 +15,38 @@ UPDATE twowheelerdata
 SET NoOfBatteries = 1
 WHERE NoOfBatteries = 0;
 
+USE `weev-dev`;
+DROP procedure IF EXISTS `GetAlltwowheelerdata`;
+
+USE `weev-dev`;
+DROP procedure IF EXISTS `weev-dev`.`GetAlltwowheelerdata`;
+;
+
+DELIMITER $$
+USE `weev-dev`$$
+CREATE DEFINER=`admin`@`%` PROCEDURE `GetAlltwowheelerdata`()
+BEGIN
+	SELECT TWId, Manufacturer, Model, Variant, VariantType, ExShowroomPrice, MaxSpeed,
+           ChargingTime, ConditionOfVehicle, Accelration0To60kmph, Accelration0To40kmph, 
+           Category, Available, OfflineOROnline, BookingSite, BookingPrice, ContinuousPower, 
+           MotorPower, RangeOfVehicle, BatteryType, BatteryCapacity, ChargingTime0To80Perc, 
+           ChargingTime0To100Perc, ChargingAtHome, NoOfBatteries, SwappableBattery, InstrumentConsole, 
+           BluetoothConnectivity, Navigation, GeoFencing, AntiTheftAlarm, USBChargingPort, 
+           UnderseatStorage, DistanceToEmptyIndicator, ChargerOutputMin, ChargerOutputMax, ChargingPoint, 
+           FastCharging, FastChargingTimeUpto80Perc, RidingModes, AdditionalFeatures, CallORSMSAlerts, MusicControl,
+           CentralLocking, CruiseControl, ExternalSpeakers, Speedometer, Tripmeter, Odometer, CarryHook, 
+           ArtificialExhaustSoundSystem, InternetConnectivity, OperatingSystem, Processor, MobileApplication, 
+           ChargingStationLocater, Gradeability, Clock, LowBatteryIndicator, BodyType, DimensionsAndCapacity, 
+           BootSpace, Width, Length, Height, SaddleHeight, GroundClearance, Wheelbase, KerbWeight, LoadCarryingCapacity,
+           TurnSignalLamp, DRLs, TopSpeed, MotorType, MotorWarrantyForMonths, MotorWarrantyForKm, DriveType, 
+           BatteryWarrantyForMonths, BatteryWarrantyForKm, WaterProofRating, SuspensionFront,
+           SuspensionRear, BrakesFront, BrakesRear, TyreSize, WheelSize, WheelsType ,OurRating ,          
+           CONCAT(twi.Path,'/',twi.Image1 ,'.jpeg') Path,VehicleType
+ 	FROM twowheelerdata twd
+    INNER JOIN main_image twi
+    ON twd.TWId=twi.TW_Ref_ID;
+END$$
+
+DELIMITER ;
+;
+
