ef2cfc264ff857e926fd1ee2e81dca372712faad
Abhyudya <167675644+GitWeev@users.noreply.github.com>
2025-04-25 14:07:23 +0530

    Db final changes
    DB changes 24 April

--- WeevServices/obj/Debug/net6.0/WeevServices.AssemblyInfo.cs
+++ WeevServices/obj/Debug/net6.0/WeevServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89707041c96246325116a72ca27907690497873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf0e89ede7ad09de2292bf82984ded34cf1f24d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
