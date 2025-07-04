b51c2aa7d2826d241d1f15cf3cb11e6d99906da9
Abhyudya Bhardwaj <abhyudya007@gmail.com>
2025-07-04 09:47:58 +0530

    price chsnges

--- WeevServices/obj/Debug/net6.0/WeevServices.AssemblyInfo.cs
+++ WeevServices/obj/Debug/net6.0/WeevServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2c9f494b7d03fd692644dafc091807825a386a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f3c59cc8e44795328cffe9afb715df6fa4dcf6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeevServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
